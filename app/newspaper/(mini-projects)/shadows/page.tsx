f5e059a4a227feccb05d5d2a9b63871967f2dadd
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2024-02-05 12:28:37 +0330

    shadows

---
+++ app/newspaper/(mini-projects)/shadows/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AnimatedEffect from './components/AnimatedEffect'
+
+type Props = {}
+
+function page({}: Props) {
+  return (
+    <div className="min-h-screen">
+      <AnimatedEffect />
+    </div>
+  )
+}
+
+export default page
