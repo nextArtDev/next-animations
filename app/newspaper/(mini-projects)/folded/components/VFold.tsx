b2916e714eccb5ac91a9338e60547858ca3189a5
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2024-04-24 14:46:28 +0330

    update

--- app/newspaper/(mini-projects)/folded/components/VFold.tsx
+++ app/newspaper/(mini-projects)/folded/components/VFold.tsx
@@ -13,12 +13,7 @@ function VFold({}: Props) {
           aliquam sollicitudin magna id molestie. Proin ut eros risus. Vivamus
           consectetur ligula maximus purus suscipit, ac feugiat metus dapibus.
         </p>
-        <p>
-          Phasellus fermentum metus vel cursus vestibulum. Phasellus eu dolor
-          sed nisi mollis vehicula nec eu tellus. Morbi faucibus convallis urna
-          eget eleifend. Aenean dictum semper porttitor. Pellentesque sed
-          pellentesque nunc, viverra euismod tortor. Fusce ut aliquam diam.
-        </p>
+
         <p>Sincerely,</p>
         <p>Senders Name</p>
       </blockquote>
