34467b73cbcac3c31ea979a2d38d812d85d12e53
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2024-05-08 23:41:16 +0330

    svg text path

---
+++ app/text-svg/page.tsx
@@ -0,0 +1,24 @@
+'use client'
+import { useEffect } from 'react'
+import Lenis from '@studio-freight/lenis'
+import Footer from './components/Footer'
+
+export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis()
+
+    function raf(time: any) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+  }, [])
+
+  return (
+    <main className="">
+      <div className="h-[100vh]"></div>
+      <Footer />
+    </main>
+  )
+}
